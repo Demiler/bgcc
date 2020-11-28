bf3b48b131033bb6287740f56a652364c6d8c2ed
demiler <seerpout@gmail.com>
2020-11-28 10:39:37 +0300

    Moved args output to separate file, added args app setup, added more exit codes

--- main.c
+++ main.c
@@ -1,43 +1,70 @@
 #include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 #include "utils/params.h"
+#include "utils/logs.h"
+#include "utils/exitCodes.h"
+#include "utils/defaults.h"
+
+const char *getFilenameExt(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if(!dot || dot == filename) return NULL;
+    return dot + 1;
+}
 
 int main(int argc, char *argv[]) {
     fillArgs(argc, argv);
 
-    printf("fileName:     %s\n", params.fileName);
-    printf("pipeFile:     %s\n", params.pipeFile);
-    printf("testFile:     %s\n", params.testFile);
+    if (!params.fileName)
+        error("no input file", ERR_NO_INPUT_FILE);
+    if (access(params.fileName, F_OK))
+        error("file %s not found", ERR_NO_SUCH_FILE, params.fileName);
+    if (params.pipeFile && access(params.pipeFile, F_OK))
+        error("file %s not found", ERR_NO_SUCH_FILE, params.pipeFile);
+    if (params.testFile && access(params.testFile, F_OK))
+        error("file %s not found", ERR_NO_SUCH_FILE, params.testFile);
 
-    printf("dir:          %s\n", params.dir);
-    printf("outName:      %s\n", params.outName);
+    if (!params.dir)
+        params.dir = defaults.dir;
 
-    printf("watch:        %d\n", params.watch);
-    printf("run:          %d\n", params.run);
-    printf("sanitizer:    %d\n", params.sanitizer);
+    if (!strcmp(params.dir, defaults.dir)) {
+        if (access(params.dir, F_OK) && mkdir(params.dir, 0666) == -1) {
+            int err = errno;
+            error("couldn't create 'build' dir. Errno: %d", ERR_MKDIR, err);
+        }
+    }
+    else if (access(params.dir, F_OK))
+        error("dir %s not found", ERR_DIR, params.dir);
 
-    printf("linkedLibs:   %d\n", params.compiler.linkedLibs);
-    printf("debug:        %d\n", params.compiler.debug);
-    printf("m32:          %d\n", params.compiler.m32);
-    printf("math:         %d\n", params.compiler.math);
-    printf("ejudge:       %d\n", params.compiler.ejudge);
-    printf("compiler args: ");
-    for (int i = 0; params.compiler.args[i]; i++)
-        printf("\"%s\" ", params.compiler.args[i]);
-    printf("\n");
+    if (!params.outName) {
+        int len = strlen(params.fileName);
+        if (len > 256)
+            error("file name is too long", ERR_LONG_FILENAME);
+        char dot = getFilenameExt(params.fileName);
+        if (!dot)
+            error("no file extension!", ERR_NO_FILE_EXT);
+        int baseNameLen = dot - params.fileName - 1;
+        strncpy(defaults.genName, params.fileName, baseNameLen); 
+        params.outName = defaults.genName;
+    }
 
-    printf("hideInput:    %d\n", params.tester.hideInput);
-    printf("dontTrimData: %d\n", params.tester.dontTrimData);
-    printf("dontTrimAnsw: %d\n", params.tester.dontTrimAnsw);
-    printf("onlyFailed:   %d\n", params.tester.onlyFailed);
+    pid_t pid = fork();
+    if (pid == -1) {
+        int err = errno;
+        error("cannot create proccess. Errno: %d", ERR_FORK, err);
+    }
 
-    printf("noMsgs:       %d\n", params.noMsgs);
-    printf("noWarnings:   %d\n", params.noWarnings);
+    if (!pid) {
+        char fullName[PATH_MAX];
+        strncpy(fullName, PATH_MAX, "%s/%s", params.dir, params.outName);
+        char *gccArgs[256];
+        gccArgs[0] = fullName;
+        gccArgs[1] = defaults.gccArgs[0];
+    }
 
-    printf("args: ");
-    for (int i = 0; params.args[i]; i++)
-        printf("\"%s\" ", params.args[i]);
-    printf("\n");
-    printf("color: %d\n", params.color);
 
     return 0;
 }
--- utils/exitCodes.h
+++ utils/exitCodes.h
@@ -2,5 +2,12 @@
 #define EXIT_CODES_H
 enum {
     ERR_MISSING_ARGUMENT = 1,
+    ERR_NO_INPUT_FILE = 2,
+    ERR_NO_SUCH_FILE = 3,
+    ERR_MKDIR = 4,
+    ERR_DIR = 5,
+    ERR_LONG_FILENAME = 6,
+    ERR_NO_FILE_EXT = 7,
+    ERR_FORK = 8,
 };
 #endif
